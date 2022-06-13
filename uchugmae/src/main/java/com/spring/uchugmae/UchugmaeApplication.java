932ec8e70091dd1c1cb81e3cb9015f0a05ea5ead
Gwangwoo Seok <skwo319@naver.com>
2022-06-13 17:28:40 +0900

    first commit

---
+++ uchugmae/src/main/java/com/spring/uchugmae/UchugmaeApplication.java
@@ -0,0 +1,13 @@
+package com.spring.uchugmae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UchugmaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UchugmaeApplication.class, args);
+	}
+
+}
---
+++ uchugmae/src/test/java/com/spring/uchugmae/UchugmaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.uchugmae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UchugmaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
